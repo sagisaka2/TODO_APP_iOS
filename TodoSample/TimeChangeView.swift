c964e7b794149298e19349d53f8d97c7b7aeefbb
匂坂 勇仁 <sagisakahayato@team-lab.com>
2018-11-30 10:51:22 +0900

    時間のピックアップ画面を作成

---
+++ TodoSample/TimeChangeView.swift
@@ -0,0 +1,74 @@
+//
+//  CategoryView.swift
+//  TodoSample
+//
+//  Created by 匂坂 勇仁 on H30/11/26.
+//  Copyright © 平成30年 TeamLab. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class TimeChangeView: UIViewController {
+    
+    var now = Date()
+    var text1 = String()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        //ここでタイトルを設定
+        self.title = "日付変更"
+        self.view.backgroundColor = UIColor.white
+        
+        let viewWidth = UIScreen.main.bounds.size.width
+        let viewHeight = UIScreen.main.bounds.size.height
+        
+        //Doneを作成
+        let doneBtn = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.doneTapped(sender:)))
+        doneBtn.tintColor = UIColor.blue
+        //ボタンを設置
+        self.navigationItem.rightBarButtonItem = doneBtn
+        
+        //UIDatePickerのインスタンスを生成
+        let datePicker = UIDatePicker()
+        
+        //datePickerの位置とサイズを設定
+        datePicker.frame = CGRect(x: 0, y: viewHeight / 2 - 150, width: viewWidth, height: 300)
+        
+        //タイムゾーンの設定
+        datePicker.timeZone = NSTimeZone.local
+        
+        //データ選択時の処理内容の設定
+        datePicker.addTarget(self, action: #selector(TimeChangeView.changeDate(sender:)), for: .valueChanged)
+        
+        //datePickerをViewに追加
+        self.view.addSubview(datePicker)
+    }
+    
+    //データ選択時の処理
+    @objc internal func changeDate(sender: UIDatePicker) {
+        print("change")
+        now = sender.date
+    }
+    
+    func getNowClockString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM月dd日' 'HH時mm分"
+        return formatter.string(from: now)
+    }
+    
+    @objc func doneTapped(sender: UIDatePicker) {
+        let secondVC = TodoCreate()
+        secondVC.text = getNowClockString()
+        secondVC.text1 = text1
+        print(getNowClockString())
+        self.navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
